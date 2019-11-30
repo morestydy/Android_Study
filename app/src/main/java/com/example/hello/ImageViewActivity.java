d97cbf75199b82b3d4b9bf1df2bb997cb86a725c
morestydy <1692125978@qq.com>
2019-12-01 03:17:40 +0800

    加载第三方包

--- app/src/main/java/com/example/hello/ImageViewActivity.java
+++ app/src/main/java/com/example/hello/ImageViewActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 public class ImageViewActivity extends AppCompatActivity {
     private ImageView mIv4;
     @Override
@@ -12,5 +14,6 @@ public class ImageViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
         mIv4 = (ImageView) findViewById(R.id.iv_4);
+        Glide.with(this).load("http://goo.gl/gEgYUd").into(mIv4);
     }
 }
